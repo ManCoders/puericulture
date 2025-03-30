bf0b3be1f471852a0386ca22967f0e15d17fb03a
Manuel D. <daligdig.manuel19@gmail.com>
2025-03-30 11:55:16 +0800

    update the nav bar

--- payroll/header.php
+++ payroll/header.php
@@ -1,5 +1,6 @@
 
 <script src="./assets/npm/chart.js"></script>
 <script src="./assets/js/custom-script.js"></script>
+<script src="./assets/js/jquery-3.6.0.min.js"></script>
 
 <link rel="stylesheet" href="./assets/css/style.css">
--- payroll/navbar.php
+++ payroll/navbar.php
@@ -1,54 +1,57 @@
-
 <!-- <?php include "header.php" ?> -->
 <link rel="stylesheet" href="./assets/css/style.css">
 
 <div class="nav_main_bar">
-
     <nav class="nav" id="sidebar">
-    <div class="logo"> <img style="width:100px; border-radius:50%;" src="./assets/images/puericulture_logo.jpg" alt="Logo"></div>
-                <a href="index.php?page=home" class="nav-home">
-                    <span>Dashboard</span>
-                </a>
-                <a href="index.php?page=payroll" class="nav-payroll">
-                    <span>Payroll Process</span>
-                </a>
-                <a href="index.php?page=salary" class="nav-salary">
-                    <span>Salary Reports</span>
-                </a>
-                <a href="index.php?page=employee" class="nav-employee">
-                    <span>Management</span>
-                </a>
-                <a href="index.php?page=setting" class="nav-setting">
-                    <span>Setting</span>
-                </a>
-                <a href="../logout.php" class="nav-setting">
-                    <span>Logout</span>
-                </a>
-
+        <div class="logo"> 
+            <img style="width:100px; border-radius:50%;" src="./assets/images/puericulture_logo.jpg" alt="Logo">
         </div>
-    </nav>
-</div>
+        
+        <?php $page = isset($_GET['page']) ? $_GET['page'] : 'home'; ?>
 
+        <a href="index.php?page=home" class="nav-item nav-home <?= ($page == 'home') ? 'active' : '' ?>">
+            <span>Dashboard</span>
+        </a>
+        <a href="index.php?page=payroll" class="nav-item nav-payroll <?= ($page == 'payroll') ? 'active' : '' ?>">
+            <span>Payroll Process</span>
+        </a>
+        <a href="index.php?page=salary" class="nav-item nav-salary <?= ($page == 'salary') ? 'active' : '' ?>">
+            <span>Salary Reports</span>
+        </a>
+        <a href="index.php?page=employee" class="nav-item nav-employee <?= ($page == 'employee') ? 'active' : '' ?>">
+            <span>Management</span>
+        </a>
+        <a href="index.php?page=setting" class="nav-item nav-setting <?= ($page == 'setting') ? 'active' : '' ?>">
+            <span>Setting</span>
+        </a>
+        <a href="../logout.php" class="nav-item">
+            <span>Logout</span>
+        </a>
+    </nav>
 </div>
 
 
 <script>
-$('.nav-<?php echo $_GET['page'] ?>').addClass('active');
-
-$('.nav a').click(function() {
-    $('.nav a').removeClass('active');
-    $(this).addClass('active');
-    $(this).css('background-color', '#d10101');
-    $(this).css('color', '#fff');
-    $(this).siblings().css('background-color', '');
-    $(this).siblings().css('color', '');
+$(document).ready(function () {
+    $('.nav-item').click(function() {
+        $('.nav-item').removeClass('active');
+        $(this).addClass('active');
+    });
 });
-
-if ($('.nav-<?php echo $_GET['page'] ?>').parent().hasClass('collapse')) {
-    const parentID = $('.nav-<?php echo $_GET['page'] ?>').parent().attr('id');
-    $(`a[href="#${parentID}"]`)
-        .removeClass('collapsed')
-        .attr('aria-expanded', true);
-    $('.nav-<?php echo $_GET['page'] ?>').parent().addClass('show');
-}
 </script>
+
+<style>
+.nav-item {
+    display: block;
+    padding: 9px;
+    text-decoration: none;
+    color: black;
+    transition: 0.3s;
+    border-radius: 10px;
+}
+
+.nav-item.active {
+    background-color:rgb(158, 4, 4) !important;
+    color: white !important;
+}
+</style>
